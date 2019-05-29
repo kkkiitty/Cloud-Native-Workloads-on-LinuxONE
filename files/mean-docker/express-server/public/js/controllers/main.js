333ea8e4e77d699eb052015c5c5a614acbe54f8e
JunhuiLee <861921648@qq.com>
2019-05-29 17:30:36 +0800

    Update main.js

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -13,6 +13,15 @@ angular.module('todoController', [])
 				$scope.todos = data;
 				$scope.loading = false;
 			});
+		
+		$scope.formData.text = "Hello World";
+		Todos.create($scope.formData)
+		// if successful creation, call our get function to get all the new todos
+		.success(function(data) {
+		$scope.loading = false;
+		$scope.formData = {}; // clear the form so our user is ready to enter another
+		$scope.todos = data; // assign our new list of todos
+		});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
